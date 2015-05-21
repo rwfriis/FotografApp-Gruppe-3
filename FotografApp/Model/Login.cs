2c3cef53bbe3a8189672e884d1a58a3e870d811e
ILoveSnax <TilSkoleBrug@live.dk>
2015-05-21 15:49:16 +0200

    Register + Log In
    Metoderne skal fin pudses med Database arbejde og error bokse

--- FotografApp/Model/Login.cs
+++ FotografApp/Model/Login.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls.Primitives;
 
 namespace FotografApp.Model
 {
     class Login
     {
+        public void LoginAsUser(string email, string password)
+        {
+            if (email.Contains("If exist in server") &&
+                password.Contains("If corresponds with the pasword of the email"))
+            {
+                //Log in
+            }
+            //Popup with text: Failed to login in: Email or password is incorrect
+        }
     }
 }
