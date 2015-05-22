dc713b63cbcecb8c1b344037428b0f80196d0e7f
ILoveSnax <TilSkoleBrug@live.dk>
2015-05-22 13:51:00 +0200

    Register Ændring
    Register kan nu checke om en bruger allerede eksistere med samme email.
    Register kan også tilføje en User med de korrekte attributter nu.

--- FotografApp/Model/User.cs
+++ FotografApp/Model/User.cs
@@ -9,8 +9,17 @@ namespace FotografApp.Model
     class User
     {
         public string Name { get; set; }
+        public string Password { get; set; }
         public string Email { get; set; }
         public string Tlf { get; set; }
         public bool TypeOfUser { get; set; }
+
+        public User(string name, string password, string email, string tlf)
+        {
+            Name = name;
+            Password = password;
+            Email = email;
+            Tlf = tlf;
+        }
     }
 }
