d0868f42b4df9205cf81ab4acfe42121d224f5ed
ILoveSnax <TilSkoleBrug@live.dk>
2015-05-22 10:13:18 +0200

    Database Handler

---
+++ FotografApp/Persistency/DatabasePersistencyHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FotografApp.Persistency
+{
+    class DatabasePersistencyHandler
+    {
+        private readonly static DatabasePersistencyHandler instance = new DatabasePersistencyHandler();
+
+        public static DatabasePersistencyHandler Instance { get { return instance; } }
+    }
+}
