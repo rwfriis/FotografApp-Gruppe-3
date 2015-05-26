d441f5661692e828201387d380df9474f0d18c32
ILoveSnax <TilSkoleBrug@live.dk>
2015-05-26 12:24:38 +0200

    Revert "Login Class"
    This reverts commit d80e74c2a738439a8f08b3dba27b74b376b1a060.

--- FotografApp/Model/Login.cs
+++ FotografApp/Model/Login.cs
@@ -1,62 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.ServiceModel;
-using System.ServiceModel.Channels;
 using System.Text;
 using System.Threading.Tasks;
-using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
-using FotografApp.Persistency;
 
 namespace FotografApp.Model
 {
     class Login
     {
-        private static bool _isLoggedIn = false;
-
-        public static bool IsLoggedIn
-        {
-            get { return _isLoggedIn; }
-            set { _isLoggedIn = value;}
-        }
-
-        public bool UserDoesNotExist { get; set; }
-        public bool PasswordIsWrong { get; set; }
-
         public void LoginAsUser(string email, string password)
         {
-            List<User> user = DatabasePersistencyHandler.Instance.GetUsers();
-            bool exist;
-            bool passwordIsTrue;
-
-            foreach (var users in user)
+            if (email.Contains("If exist in server") &&
+                password.Contains("If corresponds with the pasword of the email"))
             {
-                if (users.Email.Equals(email))
-                {
-                    exist = true;
-                    if (users.Password.Equals(password))
-                        passwordIsTrue = true;
-                }
+                //Log in
             }
-
-            if (exist == true)
-            {
-                if (passwordIsTrue == true)
-                {
-                    _isLoggedIn = true;
-                    PasswordIsWrong = false;
-                    UserDoesNotExist = false;
-                }
-                else
-                {
-                    PasswordIsWrong = true;
-                }
-            }
-            else
-            {
-                UserDoesNotExist = true;
-            }
+            //Popup with text: Failed to login in: Email or password is incorrect
         }
     }
 }
