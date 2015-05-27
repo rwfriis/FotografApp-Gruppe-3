6f5a16de6c0e082367ba55f0140c0bcd555cacf5
ILoveSnax <TilSkoleBrug@live.dk>
2015-05-27 09:58:48 +0200

    Handlers tilføjet

---
+++ FotografApp/Handler/OrderHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FotografApp.Handler
+{
+    class OrderHandler
+    {
+        private MainViewModel _viewModel { get; set; }
+
+        public OrderHandler(MainViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public void CreateOrder()
+        {
+            
+        }
+
+        public void DeleteOrder()
+        {
+            
+        }
+
+        public void UpdateOrder()
+        {
+            
+        }
+    }
+}
